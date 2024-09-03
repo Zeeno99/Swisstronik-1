26a198f24dc2cbe0e7683594a8bdb74881108661
Zino Oteri <ewomazinooteri@gmail.com>
2024-09-03 12:44:57 +0000

    Initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [""],
+      accounts: ["udget enact cream twenty impose window patrol unique music expire survey close"],
     },
   },
 };
